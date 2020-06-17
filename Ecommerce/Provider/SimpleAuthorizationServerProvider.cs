ac464a3d350981c25e71f1605327c8293e97692e
Hamee <sahulhameed@jetzerp.com>
2020-06-17 09:30:49 +0530

    initial commit

---
+++ Ecommerce/Provider/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.OAuth;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+
+using System.Threading.Tasks;
+using System.Web.Http.Cors;
+using Microsoft.Owin.Security.Infrastructure;
+using System;
+using System.Collections.Concurrent;
+using System.Data.Entity;
+using System.Net.Http;
+using System.Web;
+
+namespace TokenAuthentication.Provider
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated(); //   
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            
+            /*var identity1 = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity1.AddClaim(new Claim("sub", context.UserName));
+            identity1.AddClaim(new Claim(ClaimTypes.Role, "user"));
+
+            context.Validated(identity1);*/
+        }
+    }
+
+    public class RefreshTokenProvider : IAuthenticationTokenProvider
+    {
+        private static ConcurrentDictionary<string, AuthenticationTicket> _refreshTokens = new ConcurrentDictionary<string, AuthenticationTicket>();
+        public async Task CreateAsync(AuthenticationTokenCreateContext context)
+        {
+            var guid = Guid.NewGuid().ToString();
+
+            // copy all properties and set the desired lifetime of refresh token  
+            var refreshTokenProperties = new AuthenticationProperties(context.Ticket.Properties.Dictionary)
+            {
+                IssuedUtc = context.Ticket.Properties.IssuedUtc,
+                ExpiresUtc = DateTime.UtcNow.AddMinutes(1200000)//DateTime.UtcNow.AddYears(1)  
+            };
+            var refreshTokenTicket = new AuthenticationTicket(context.Ticket.Identity, refreshTokenProperties);
+
+            _refreshTokens.TryAdd(guid, refreshTokenTicket);
+
+            // consider storing only the hash of the handle  
+            context.SetToken(guid);
+        }
+
+        public void Create(AuthenticationTokenCreateContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Receive(AuthenticationTokenReceiveContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task ReceiveAsync(AuthenticationTokenReceiveContext context)
+        {
+            AuthenticationTicket ticket;
+            string header = context.OwinContext.Request.Headers["Authorization"];
+
+            if (_refreshTokens.TryRemove(context.Token, out ticket))
+            {
+                context.SetTicket(ticket);
+            }
+        }
+    }
+}
