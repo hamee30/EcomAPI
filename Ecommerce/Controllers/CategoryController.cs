f065c1a2689a8ea1a77c31859b87b1cf2c780b97
Hamee <sahulhameed@jetzerp.com>
2020-06-17 19:52:56 +0530

    Entity Model

---
+++ Ecommerce/Controllers/CategoryController.cs
@@ -0,0 +1,46 @@
+﻿using Ecommerce.Models;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Ecommerce.Controllers
+{
+    public class CategoryController : ApiController
+    {
+
+        ecommEntities context = new ecommEntities();
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("api/Category/GetCategoryList")]
+        public JObject GetCategoryList()
+        {
+            JObject details;
+            JArray usr = new JArray();
+            var emailList = context.Categories.AsNoTracking();
+            int i = 1;
+            foreach (Category bk in emailList)
+            {
+                details = new JObject(
+                   new JProperty("s_no", i++),
+                   new JProperty("Id", bk.Id),
+                   new JProperty("Name", bk.category_name)
+                  );
+
+                usr.Add(details);
+
+            }
+
+            JObject final = new JObject(
+              new JProperty("CategoryList", usr));
+            return final;
+
+
+
+        }
+
+        }
+}
